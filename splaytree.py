418ba4687d8e6fb2fc9d8fb00f597600ea2da7be
Caleb Levy <cclevy@princeton.edu>
2016-07-20 19:36:02 -0400

    Added greatest upper bound, comments on why we can't splay directly to the successor

--- splaytree.py
+++ splaytree.py
@@ -8,16 +8,58 @@ import unittest
 @functools.total_ordering
 class _GlobalMax(object):
     """Represent a global maximum."""
+    __slots__ = ()
 
     def __lt__(self, other):
         return False
 
 
+@functools.total_ordering
 class _GlobalMin(object):
     """Represent a global minimum."""
+    __slots__ = ()
 
     def __lt__(self, other):
         return True
+
+
+# Cannot use functools.total_ordering; LUB objects are not totally ordered
+class LUB(object):
+    """LUB(x) is the least upper bound of x. y < LUB(x) <==> y <= x."""
+    __slots__ = ("_x")
+
+    def __init__(self, x):
+        self._x = x
+
+    def __lt__(self, other):
+        return self._x < other
+
+    def __le__(self, other):
+        return self._x < other
+
+    def __ge__(self, other):
+        return self._x >= other
+
+    def __gt__(self, other):
+        return self._x >= other
+
+    def __eq__(self, other):
+        return NotImplemented
+
+    def __ne__(self, other):
+        return NotImplemented
+
+
+# @functools.total_ordering
+# def GLB(object):
+#     """GLB(x) is the greatest lower bound of x. GLB(x) < y <==> x <= y."""
+#     __slots__ = ("_x")
+#
+#     def __init__(self, x):
+#         self._x = x
+#
+#     def __lt__(self, other):
+#         return self._x <= other
 
 
 Inf = _GlobalMax()
@@ -186,12 +228,78 @@ class SplayTree(object):
             return
         min = 1
 
+    # My idea of "successor" requires at least four things.
+    # 1) I will define iteration over the tree by repeated calls to successor
+    # 2) I need to be able to access, insert and delete items DURING iteration
+    # 3) I need to guarentee at least the O(log n) time bounds, homefully the
+    #    other theorems as well
+    # 4) In particular, if the sequential access theorem holds for simple
+    #    splaying, then iterating in symmetric order should require no more
+    #    than linear time assuming no other access/insert/delete during the
+    #    loop.
+    # 5) It should not require parent pointers if at all possible.
+    #
+    # For the 3rd point, since insertion can/will allow us to increase the size
+    # of the tree as we iterate, or for that matter we can delete and add back
+    # our current node, I merely require that at each step in the iteration,
+    # the next key yielded is the successor of the previous yielded key at the
+    # time next() is called.
+    #
+    # There are several ways of iterating the tree in symmetric order, so I
+    # will outline mine, and their advantages/disadvantages to red-black trees.
+    # First, the recursive solution is out. Using recursion in production
+    # environments is already usually not the best choice, but we can usually
+    # get away with it in BALANCED binary trees since they guarentee
+    # logorithmic height, and so we generally won't cause a stack overflow with
+    # the number of nodes we can store in memory (or even on disk).
+    #
+    # Iteration is out, as is recursion, even with parent pointers, first for
+    # failing to meet (2), and then for more subtle reasons. Even with the
+    # parent pointers, the structure of splay trees does not guarentee that,
+    # should we access elements in the iteration, that we will have linear
+    # time to travel between. Imagine an adversary lining up the path at each
+    # step. (should see if example can be constructed).
+    #
+    # Now, I would like to just "top-down splay" directly from i.key to
+    # (i+1).key just like we do when we want to find a key. Now we don't even
+    # know what key we are looking for, which is a problem. See, not knowing
+    # "where" a key is is not the problem, but knowing "when to stop".
+    #
+    # Even for the case of finding the max or min, we can actually be more
+    # clever than Sleater (not easy to do!) and search for a key defined to be
+    # larger than all others. Why not just define a "supremum" of x.key, return
+    # sup(x)>y iff y>x?
+    #
+    # The problem is illustrated by this great program:
+    # http://stackoverflow.com/a/6335263. Probably my favorite of those that
+    # failed because its constant space, O(log n) time, and does not requre
+    # finding the key itself initially.
+    #
+    # The very issue is in the single constant it stores. Search for 13 in
+    #
+    #    20
+    #   /
+    #  10
+    #   \
+    #   11
+    #    \
+    #    12
+    #     \
+    #     13
+    #
+    # We won't know that we've hit the right node until we go all the way down
+    # the right branch at 10. We then need to either go down twice to find the
+    # key and then splay (for guarentees), or splay multiple times (no
+    # guarentees).
+    #
+    # The Inf and NegInf do not face this problem since they are not it left or
+    # right subtrees.
+
     def successor(self, key):
         """Find the smallest element greater than key."""
         if not self:
             raise KeyError("Empty tree has no successor")
         self.splay_plus(key)
-        
 
 
 class TestSimpleSplay(unittest.TestCase):
@@ -232,15 +340,15 @@ class TestSimpleSplay(unittest.TestCase):
 
         self.assertEqual(39136, t.root.right.left.right.key)
 
-        t.splay_plus(-5)
-        print(t.root.key)
-        150000 in t
-        t.splay_plus(2)
-        print(t.root.key)
-        t.splay_plus(Inf)
-        print(t.root.key)
-        t.splay_plus(2)
-        print(t.root.key)
+        # t.splay_plus(-5)
+        # print(t.root.key)
+        # 150000 in t
+        # t.splay_plus(2)
+        # print(t.root.key)
+        # t.splay_plus(Inf)
+        # print(t.root.key)
+        # t.splay_plus(2)
+        # print(t.root.key)
 
     def test_tree_truthiness(self):
         """Test tree is False iff it is empty."""
@@ -251,6 +359,42 @@ class TestSimpleSplay(unittest.TestCase):
         self.assertFalse(not r)
         self.assertFalse(s)
         self.assertTrue(not s)
+
+
+class TestExtrema(unittest.TestCase):
+
+    def test_least_upper_bound(self):
+        a = LUB(7)
+        # Test when called by reflected methods
+        self.assertTrue(7 < a)
+        self.assertTrue(7 <= a)
+        self.assertFalse(7 > a)
+        self.assertFalse(7 >= a)
+        # Test regular methods
+        self.assertTrue(a > 7)
+        self.assertTrue(a >= 7)
+        self.assertFalse(a < 7)
+        self.assertFalse(a <= 7)
+        # Test reflected methods of upper bound
+        self.assertTrue(8 > a)
+        self.assertTrue(8 >= a)
+        self.assertFalse(8 < a)
+        self.assertFalse(8 <= a)
+        # Test regular methods of upper bound
+        self.assertTrue(a < 8)
+        self.assertTrue(a <= 8)
+        self.assertFalse(a > 8)
+        self.assertFalse(a >= 8)
+        # Test reflected methods of lower bound
+        self.assertTrue(6 < a)
+        self.assertTrue(6 <= a)
+        self.assertFalse(6 > a)
+        self.assertFalse(6 >= a)
+        # Test regular methods of lower bound
+        self.assertTrue(a > 6)
+        self.assertTrue(a >= 6)
+        self.assertFalse(a < 6)
+        self.assertFalse(a <= 6)
 
 
 if __name__ == '__main__':
