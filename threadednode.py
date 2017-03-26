00c7a3e9b324c3531dad90477883bd25e705b325
Caleb Levy <cclevy@princeton.edu>
2017-03-26 16:11:39 -0400

    Add lots of ThreadedNode unittests

--- threadednode.py
+++ threadednode.py
@@ -108,12 +108,12 @@ class ThreadedNode(object):
 
     @left.setter
     def left(x, y):
-        if y.parent is not None:
+        if not isinstance(y, x.__class__):
+            raise TypeError("Left child must be of type ThreadedNoded")
+        elif y.parent is not None:
             raise ValueError("Node y already has parent")
         elif x <= y:
             raise ValueError("Left child must be less than x")
-        elif not isinstance(y, x.__class__):
-            raise TypeError("Left child must be of type ThreadedNoded")
         del x.left  # takes care of x's left pointers
         z = x.right
         if z is None:
@@ -124,12 +124,12 @@ class ThreadedNode(object):
 
     @right.setter
     def right(x, y):
-        if y.parent is not None:
+        if not isinstance(y, x.__class__):
+            raise TypeError("Right child must be of type ThreadedNode")
+        elif y.parent is not None:
             raise TypeError("Node y already has parent")
         elif y <= x:
             raise ValueError("Right child must be greater than x")
-        elif not isinstance(y, x.__class__):
-            raise TypeError("Right child must be of type ThreadedNode")
         del x.right
         z = x.left
         if z is None:
@@ -139,5 +139,63 @@ class ThreadedNode(object):
         y.side = x
 
 
-a = ThreadedNode(5)
-del a.a
+class TestThreadedNode(unittest.TestCase):
+
+    def test_ordering(self):
+        """Test nodes all behave in correct fashion."""
+        a = ThreadedNode(5)
+        b = ThreadedNode(7)
+        c = ThreadedNode(5)
+        self.assertTrue(a < b)
+        self.assertFalse(b < a)
+        self.assertTrue(a <= b)
+        self.assertFalse(b <= a)
+        self.assertTrue(a == c)
+        self.assertFalse(a is c)
+        # Compare with other objects
+        self.assertTrue(a < 7)
+        self.assertFalse(7 < a)
+        self.assertTrue(5 < b)
+        self.assertFalse(b < 5)
+        self.assertTrue(a <= 7)
+        self.assertFalse(7 <= a)
+
+    def test_left_right_insert(self):
+        """Ensure ThreadedNode has correct left/right pointers."""
+        a = ThreadedNode(5)
+        b = ThreadedNode(7)
+        c = ThreadedNode(3)
+        d = ThreadedNode(6)
+        e = ThreadedNode(4)
+        a.right = b
+        a.right.left = d
+        a.left = c
+        a.left.right = e
+        self.assertTrue(a.right is b)
+        self.assertTrue(a.right.left is d)
+        self.assertTrue(a.right.right is None)
+        self.assertTrue(a.right.left.left is None)
+        self.assertTrue(a.left is c)
+        self.assertTrue(a.left.right is e)
+        self.assertTrue(a.left.left is None)
+        self.assertTrue(a.left.right.left is None)
+        # Test parents
+        self.assertTrue(a.parent is None)
+        self.assertTrue(b.parent is a)
+        self.assertTrue(d.parent is b)
+        self.assertTrue(c.parent is a)
+        self.assertTrue(e.parent is c)
+        b_new = ThreadedNode(8)
+        a.right = b_new
+        # Test on removed block b
+        self.assertTrue(b.left is d)
+        self.assertTrue(b.parent is None)
+        self.assertTrue(d.parent is b)
+        # Test on old block
+        self.assertTrue(c.parent is a)
+        self.assertTrue(b_new.parent is a)
+        self.assertTrue(a.right.left is None)
+
+
+if __name__ == '__main__':
+    unittest.main()
