d8ac414b851ed43cf23956fa4ee56d6d92ccb3e7
Caleb Levy <cclevy@princeton.edu>
2017-04-05 13:12:02 -0400

    Verified top-down splay behaves as exected in simple splay cases

--- test_tdsplay.py
+++ test_tdsplay.py
@@ -3,8 +3,9 @@ implement, and not based on reference implementation."""
 
 import unittest
 
-from topdownsplay import TDSplayTree, BinaryNode
+from topdownsplay import TDSplayTree, BinaryNode, SimpleSplayTree
 from propersplay import SplayTree
+from treerank import randtree
 
 
 def _tree_from_preorder(preorder):
@@ -19,13 +20,62 @@ def _tree_from_preorder(preorder):
         return root
 
 
-def tdpreorder(preorder):
+def tdfrompre(preorder):
     root = _tree_from_preorder(preorder)
     T = TDSplayTree()
     T.root = root
     return T
 
 
+def stdfrompre(preorder):
+    root = _tree_from_preorder(preorder)
+    T = SimpleSplayTree()
+    T.root = root
+    return T
+
+
+def splay(T, x):
+    x in T
+
+
 class TestTDSplay(unittest.TestCase):
 
-    pass
+    def test_combinat_explosion(self):
+        """Test cases where zig-zig and zig-zags are cut short."""
+        empty = TDSplayTree()
+        self.assertTrue(1 not in empty)
+        # Tests zig-zig and zig cases identical to simple splay
+        for n in range(1, 99):  # Six gets interesting
+            r_nodes = range(n, 0, -1)
+            l_nodes = list(reversed(r_nodes))
+            td_r = tdfrompre(r_nodes)
+            std_r = stdfrompre(r_nodes)
+            td_l = tdfrompre(l_nodes)
+            std_l = tdfrompre(l_nodes)
+            if n <= 5:
+                for k in l_nodes:
+                    splay(td_r, k)
+                    splay(std_r, k)
+                    self.assertTrue(std_r.preorder() == td_r.preorder())
+                for k in r_nodes:
+                    splay(td_l, k)
+                    splay(std_l, k)
+                    self.assertTrue(std_l.preorder(), td_l.preorder())
+            else:
+                splay(td_r, 1)
+                splay(std_r, 1)
+                self.assertEqual(std_r.preorder(), td_r.preorder())
+                splay(td_r, 2)
+                splay(std_r, 2)
+                self.assertNotEqual(td_r.preorder(), std_r.preorder())
+                # Test reflected
+                splay(td_l, n)
+                splay(std_l, n)
+                self.assertEqual(std_l.preorder(), td_l.preorder())
+                splay(td_l, n-1)
+                splay(std_l, n-1)
+                self.assertEqual(std_l.preorder(), td_l.preorder())
+
+
+if __name__ == '__main__':
+    unittest.main()
