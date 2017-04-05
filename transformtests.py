50673c4ef32ada85baa683a9179ff3755c71748c
Caleb Levy <cclevy@princeton.edu>
2017-04-05 12:31:47 -0400

    Cleanup transformtests.py

--- transformtests.py
+++ transformtests.py
@@ -18,28 +18,6 @@ four_node_trees = [
     (4, 2, 1, 3), (1, 3, 2, 4),  # Yl, Yr
 ]
 
-def _tree_from_preorder(preorder):
-    """Slow way to build a tree from the preorder permutation p."""
-    if preorder:
-        root_item = preorder[0]
-        left_items = [item for item in preorder if item < root_item]
-        right_items = [item for item in preorder if item > root_item]
-        root = Node(root_item)
-        root.left = _tree_from_preorder(left_items)
-        root.right = _tree_from_preorder(right_items)
-        if root.left is not None:
-            root.left.parent = root
-        if root.right is not None:
-             root.right.parent = root
-        return root
-
-
-def tree_from_preorder(p):
-    """Return splay tree with preorder sequence P."""
-    T = SplayTree()
-    T.root = _tree_from_preorder(p)
-    return T
-
 
 def weighted_diameter(G):
     """Wrapper for graph diameter of weighted networkx graph."""
@@ -60,41 +38,24 @@ def weighted_diameter(G):
     return maxcost
 
 
-def hamilton(G):
-    """Determine if graph has hamiltonian cycle"""
-    F = [(G,[G.nodes()[0]])]
-    n = G.number_of_nodes()
-    while F:
-        graph,path = F.pop()
-        confs = []
-        for node in graph.neighbors(path[-1]):
-            conf_p = path[:]
-            conf_p.append(node)
-            conf_g = nx.Graph(graph)
-            conf_g.remove_node(path[-1])
-            confs.append((conf_g,conf_p))
-        for g,p in confs:
-            if len(p)==n:
-                return p
-            else:
-                F.append((g,p))
-    return None
-
-
 def splaytransformtests(n):
     """Do tests for connectedness, hamiltonian, weight."""
     G = nx.DiGraph()
     for p in treegen(n):
         for x in p[1:]:
-            t = tree_from_preorder(p)
+            t = SplayTree(p)
             t.access(x)
             G.add_edge(p, t.preorder(), weight=t.count)
     print("Splay stats for n=%s:" % n)
     print("----------------------")
     print("B(%s) = %s" % (n, B(n)))
+    scc = nx.is_strongly_connected(G)
     print("Is strongly connected: %s" % nx.is_strongly_connected(G))
-    print("Access Overhead: %s" % nx.diameter(G))
-    print("Cost Overhead: %s" % weighted_diameter(G))
+    if scc:
+        print("Access Overhead: %s" % nx.diameter(G))
+        print("Cost Overhead: %s" % weighted_diameter(G))
+    else:
+        G = nx.condensation(G)
     pos = nx.shell_layout(G)
     nx.draw_networkx_nodes(G, pos, node_size=700)
     nx.draw_networkx_edges(G, pos, width=1)
@@ -103,4 +64,4 @@ def splaytransformtests(n):
     plt.savefig("weighted_graph.png") # save as png
     return G
 
-splaytransformtests(4)
+splaytransformtests(7)
