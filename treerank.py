2ec207ea7bbaee7eccc07eacf8b7f15803d500d0
Caleb Levy <cclevy@princeton.edu>
2017-04-11 08:21:22 -0400

    Add unit tests to treerank, clean up

--- treerank.py
+++ treerank.py
@@ -1,6 +1,8 @@
 """Functions based on "A numbering system for binary trees," Gary Knott,
 1977."""
 
+
+import unittest
 from random import randrange
 
 
@@ -8,8 +10,9 @@ class memoize:
     def __init__(self, f):
         self.f = f
         self.memo = {}
+
     def __call__(self, *args):
-        if not args in self.memo:
+        if args not in self.memo:
             self.memo[args] = self.f(*args)
         return self.memo[args]
 
@@ -49,11 +52,44 @@ def irank(i, n):
 
 def treegen(n):
     """Generate BST preorders on n nodes."""
-    for i in range(1, B(n)+1):
-        yield tuple(irank(i, n))
+    if n:
+        for i in range(1, B(n)+1):
+            yield tuple(irank(i, n))
+    else:
+        yield ()
 
 
 def randtree(n):
     """Generate random BST on n nodes."""
     i = randrange(1, B(n)+1)
     return irank(i, n)
+
+
+class TestTreeranks(unittest.TestCase):
+
+    def test_catalan(self):
+        """Test B(n) and treegen are in agreement on OEIS A000108."""
+        catalan = [1, 1, 2, 5, 14, 42, 132, 429, 1430]
+        for i, b in enumerate(catalan):
+            self.assertEqual(b, B(i))
+            trees = list(treegen(i))
+            self.assertEqual(b, len(trees))
+            self.assertEqual(b, len(set(trees)))
+
+    def test_preorders(self):
+        """Test by brute force on lenth 4."""
+        four_node_trees = [
+            (1, 2, 3, 4), (1, 2, 4, 3),
+            (1, 3, 2, 4), (1, 4, 2, 3),
+            (1, 4, 3, 2), (2, 1, 3, 4),
+            (2, 1, 4, 3), (3, 1, 2, 4),
+            (3, 2, 1, 4), (4, 1, 2, 3),
+            (4, 1, 3, 2), (4, 2, 1, 3),
+            (4, 3, 1, 2), (4, 3, 2, 1)
+        ]
+        self.assertEqual(four_node_trees, list(treegen(4)))
+        self.assertEqual(sorted(list(treegen(6))), list(treegen(6)))
+
+
+if __name__ == '__main__':
+    unittest.main()
