b79607f2ad64626aa40e6c2e17d0f25446cc40c5
Caleb Levy <cclevy@princeton.edu>
2017-07-04 13:02:46 -0700

    Add outline to Supervisor function

--- subramanian.py
+++ subramanian.py
@@ -1,7 +1,14 @@
 """Implementation of Supervisor-based local bottom up splay-like algorithms."""
 
-def Supervisor(Ruleset):
+def Supervisor(Ruleset, doc=None):
     """Output splay-like algorithm given Ruleset[Template, Result].
     Assumes Ruleset is applied to longest subpath applicable."""
     H_max = max(map(len, Ruleset.keys())) - 1
-    
+    def apply_rule(node):
+        for i in range(H_max, -1 -1):
+            if encodestring in Ruleset:
+                ...
+    def algorithm(x):
+        while x not parent:
+            x = apply_rule
+        return x
