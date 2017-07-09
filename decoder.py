4735f5ee0402ad02e5a716c85ea55316641e5e1f
Caleb Levy <cclevy@princeton.edu>
2017-07-09 09:53:01 -0700

    Add unit tests for splay decoder

--- decoder.py
+++ decoder.py
@@ -21,7 +21,7 @@ class TestDecoder(unittest.TestCase):
     """Test decoder works as expected."""
 
     def test_static_decoder(self):
-        """Test encoding of decoder is same."""
+        """Test encoding of static tree is same."""
         code_1 = ["1", "111", "11111", "1", "11111"]
         t, nodes = decoder(code_1, static)
         self.assertTrue(t.is_isomorphic_to(Node.from_cursor('r'*5)))
@@ -32,6 +32,21 @@ class TestDecoder(unittest.TestCase):
         self.assertTrue(t.is_isomorphic_to(Node.from_cursor('llrppprlr')))
         for e, node in zip(code_2, nodes):
             self.assertTrue(e == node.encode())
+
+    def test_splay_decoder(self):
+        """Test encoding of splay trees."""
+        code_1 = ["1", "111", "11111", "1", "11111"]
+        t, nodes = decoder(code_1)
+        self.assertTrue(t.is_isomorphic_to(Node.from_cursor('r'*15)))
+        for e, node in zip(code_1, nodes):
+            self.assertTrue(e == node.encode())
+            node.splay()
+        code_2 = ["0", "10", "001", "101"]
+        t, nodes = decoder(code_2)
+        self.assertTrue(t.is_isomorphic_to(Node.from_cursor('llrpprl')))
+        for e, node in zip(code_2, nodes):
+            self.assertTrue(e == node.encode())
+            node.splay()
 
 
 if __name__ == '__main__':
