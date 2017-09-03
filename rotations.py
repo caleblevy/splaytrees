7303e9e1fa126d16cd68ea9a485339b902394213
Caleb Levy <cclevy@princeton.edu>
2017-09-03 14:21:49 -0700

    Add Move-to-root rotations

--- rotations.py
+++ rotations.py
@@ -29,6 +29,22 @@ class RotationTracker(Node):
                 yield (x, x.parent)
                 x.rotate()
 
+    @maker(tuple)
+    def move_to_root(x):
+        """Proper bottom-up splay to the top."""
+        while x.parent is not None:
+            yield (x, x.parent)
+            x.rotate()
+
+
+@maker(tuple)
+def access_path(x):
+    """Return path of nodes from x to the root."""
+    yield x
+    while x.parent is not None:
+        x = x.parent
+        yield x
+
 
 def splay_rotations(s):
     """Rotations performed by splay operations starting from the right path."""
@@ -44,6 +60,30 @@ def splay_rotations(s):
     return R
 
 
-print splay_rotations(range(16, 0, -1))
-for rots in splay_rotations([1, 9, 8, 10, 7, 6, 3, 12, 11, 5, 14, 4, 2, 16, 13, 15, 9]):
-    print(rots)
+def mr_rotations(s):
+    """Rotations performed by splay operations starting from the right path."""
+    q = sorted(set(s))
+    n = len(q)
+    t = RotationTracker.from_cursor("r"*(n-1))
+    d = dict(zip(q, t.preorder()))
+    d_inv = {node: key for key, node in d.items()}
+    R = []
+    for x in s:
+        rots = d[x].move_to_root()
+        R.append(list((d_inv[x], d_inv[y]) for x, y in rots))
+    return R
+
+
+def splay_paths(s):
+    """Compute the splay paths."""
+    
+
+
+# print splay_rotations(range(16, 0, -1))
+# for rots in splay_rotations([1, 9, 8, 10, 7, 6, 3, 12, 11, 5, 14, 4, 2, 16, 13, 15, 9]):
+#     print(rots)
+# print(splay_rotations([1,2,3]))
+# for rots in mr_rotations([1, 9, 8, 10, 7, 6, 3, 12, 11, 5, 14, 4, 2, 16, 13, 15, 9]):
+#     print(rots)
+print mr_rotations(range(16,0,-1))
+print mr_rotations([1,2,3])
