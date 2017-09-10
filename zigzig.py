66a61dfb15fd6ca38aadadc9003b3e9dc986bd07
Caleb Levy <cclevy@princeton.edu>
2017-09-10 11:33:14 -0700

    Code to test and compute zig-zig depths of nodes

--- zigzig.py
+++ zigzig.py
@@ -36,6 +36,26 @@ def zagzig_depths(x):
                 d[k[y]] = d[k[z]]
     return d
 
+
+def zigzig_depths(x):
+    """Returns zig-zag depths of nodes of subtree rooted at x."""
+    d = {}
+    k = x.node_to_key()
+    p = iter(x.preorder())
+    d[k[next(p)]] = 0
+    for y in p:
+        z = y.parent
+        if z.parent is x.parent:
+            d[k[y]] = 0
+        else:
+            w = z.parent
+            if (z is w.right and y is z.right) or (z is w.left and y is z.left):
+                d[k[y]] = d[k[z]] + 1
+            else:
+                d[k[y]] = d[k[z]]
+    return d
+
+
 def tree_nodes(movements):
     """Return mapping of 1...n to the nodes described by the cursor
     movements."""
@@ -87,6 +107,28 @@ class TestDepths(unittest.TestCase):
         sub_depths = zagzig_depths(t[14])
         self.assertEqual(0, sub_depths[5])
         self.assertEqual(1, sub_depths[3])
+        self.assertEqual(0, sub_depths[2])
+
+    def test_zigzig_depths(self):
+        """Test number of zig-zigs on splay paths is correct."""
+        d = zigzig_depths(Node())
+        self.assertEqual({1: 0}, d)
+        t = tree_nodes("r"*30)
+        rp_depths = zigzig_depths(t[1])
+        for k in range(1, 31):
+            self.assertEqual(max(0, k-2), rp_depths[k])
+        t[31].splay()
+        t[16].splay()
+        t[28].splay()
+        t_depths = zigzig_depths(t[28])
+        self.assertEqual(3, t_depths[14])
+        self.assertEqual(3, t_depths[12])
+        self.assertEqual(4, t_depths[11])
+        self.assertEqual(0, t_depths[30])
+        self.assertEqual(0, t_depths[17])
+        sub_depths = zigzig_depths(t[22])
+        self.assertEqual(1, sub_depths[5])
+        self.assertEqual(0, sub_depths[7])
 
 
 if __name__ == '__main__':
