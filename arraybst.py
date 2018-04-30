f6d2f2137e0c238f7402ccfcfcf9364b41c7e51b
Caleb Levy <clevy@intertrust.com>
2018-04-29 21:45:54 -0700

    ArrayBst looking more and more like BST

--- arraybst.py
+++ arraybst.py
@@ -19,18 +19,24 @@ def _isright(ind):
     return (ind-4) % 4
 
 
+root = null = 0
+parent = 1
+left = 2
+right = 3
+
+
 class ArrayTree(object):
     """Tree stored with each "node" is contiguous "key, parent, left, right"
     eg.
         [None, "b", 0, 9, 5, "c", 1, 0, 0, "a", 1, 0, 0]
-    is equivalent to 
+    is equivalent to
         b
        / \
       a  c
     """
 
     def __init__(T, items=None):
-        T._arr = [None]
+        T._arr = [null]
         if items is not None:
             for key in items:
                 T.insert(key)
@@ -38,25 +44,28 @@ class ArrayTree(object):
     def __getitem__(T, ind):
         return T._arr[ind]
 
+    def __setitem__(T, ind, v):
+        T._arr[ind] = v
+
     def _find(T, key):
         """Return index of key, if found."""
-        if len(T._arr) == 1:
-            return 0
-        ind = 1
-        while T[ind] is not None:
-            prev_ind = ind
-            if key < T[ind]:
-                ind = T[ind + 2]
-            elif key > T[ind]:
-                ind = T[ind + 3]
+        x = T[root]
+        if x == null:
+            return null
+        while x != null:
+            y = x
+            if key < T[x]:
+                x = T[x + left]
+            elif key > T[x]:
+                x = T[x + right]
             else:
                 break
-        if key > T[prev_ind]:
-            return prev_ind + 3
-        elif key < T[prev_ind]:
-            return prev_ind + 2
+        if key > T[y]:
+            return y + right
+        elif key < T[y]:
+            return y + left
         else:
-            return prev_ind
+            return y
 
     def find(T, key):
         """See if key is in T."""
@@ -68,16 +77,37 @@ class ArrayTree(object):
         if _iskey(ind):
             return
         node = [key, 0, 0, 0]
+        T[ind] = len(T._arr)
         if ind != 0:
-            T._arr[ind] = len(T._arr)
             direction = (ind - 1) % 4
-            parent = ind - direction
-            node[1] = parent
+            p = ind - direction
+            node[1] = p
         T._arr.extend(node)
 
     def _rotate(T, ind):
-        parent = T[ind + 1]
-        
+        x = ind
+        y = T[ind + parent]
+        assert y != 0  # Do not rotate root
+        if x is T[y + right]:
+            x, y = y, x
+        if x is T[y + left]:
+            # Shift around subtree
+            w = T[x + right]
+            T[y + left] = w
+            if w != 0:
+                T[w + parent] = y
+            # Switch up parent pointers
+            z = T[y + parent]
+            T[x + parent] = z
+            # y is the root
+            if z != 0:
+                if y is T[z + right]:
+                    T[z + right] = x
+                else:
+                    T[z + left] = x
+            else:
+                T[0] = x
+            
 
 
 class TestArrayTree(unittest.TestCase):
@@ -110,7 +140,7 @@ class TestArrayTree(unittest.TestCase):
         T2 = ArrayTree()
         T2.insert(4)
         self.assertFalse(ArrayTree().find(7))
-        self.assertEqual([None, 4, 0, 0, 0], T2._arr)
+        self.assertEqual([1, 4, 0, 0, 0], T2._arr)
 
 
 if __name__ == '__main__':
