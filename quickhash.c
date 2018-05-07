b53f63ebd8945e7f11119bd078a7debba0782ead
Caleb Levy <clevy@intertrust.com>
2018-05-06 17:00:11 -0700

    Add quickhash from Stack Overflow

---
+++ quickhash.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+
+unsigned int hash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
+unsigned int unhash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
+uint64_t hash64(uint64_t x) {
+    x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
+    x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
+    x = x ^ (x >> 31);
+    return x;
+}
+
+uint64_t unhash64(uint64_t x) {
+    x = (x ^ (x >> 31) ^ (x >> 62)) * UINT64_C(0x319642b2d24d8ec3);
+    x = (x ^ (x >> 27) ^ (x >> 54)) * UINT64_C(0x96de1b173f119089);
+    x = x ^ (x >> 30) ^ (x >> 60);
+    return x;
+}
+
+int main (int argc, char const *argv[])
+{
+	printf("%u\n", hash(4));
+	printf("%u\n", hash(5));
+	printf("%u\n", unhash(hash(5)));
+	printf("%llu\n", hash64(10));
+	printf("%llu", unhash64(hash64(4)));
+	return 0;
+}
