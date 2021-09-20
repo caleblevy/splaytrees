6550722c54c61847d8e4f06fc206fe6044b2252c
Caleb Levy <caleb.levy@gmail.com>
2021-09-20 10:16:34 -0700

    Works

--- randombst.py
+++ randombst.py
@@ -9,27 +9,11 @@ class Node:
 
     def __init__(self, value=None):
         self.value = value
-        self.parent = None
         self.left = None
         self.right = None
-        self._direction = LEFT
 
-    def insert_left(self, value=None):
-        assert self.left is None
-        node = Node(value)
-        node.parent = self
-        self.left = node
-        return node
-
-    def insert_right(self, value=None):
-        assert self.right is None
-        node = Node(value)
-        node.parent = self
-        self.right = node
-        return node
-
-    def inorder(self):
-        return list(inorder(self))
+    def preorder(self):
+        return list(preorder(self))
 
 
 def relabel(node, start=0):
@@ -41,11 +25,11 @@ def relabel(node, start=0):
     
 
 
-def inorder(node):
+def preorder(node):
     if node is not None:
-        yield from inorder(node.left)
         yield node.value
-        yield from inorder(node.right)
+        yield from preorder(node.left)
+        yield from preorder(node.right)
 
 
 def _generate_balanced_string(n):
@@ -63,45 +47,38 @@ def _generate_balanced_string(n):
 
 
 def random_balanced_string(n):
-    return "".join("(" if p else ")" for p in _generate_balanced_string(n))
+    return "".join("(" if c else ")" for c in _generate_balanced_string(n))
 
 
 def random_bst(n):
-    current = None
-    for c in _generate_balanced_string(n):
-        print(c)
-        if current is not None:
-            print(current.parent)
-        if c:
-            if current is None:
-                root = current = Node()
-            elif not current._left_complete:
-                current = current.insert_left()
-            else:
-                current = current.insert_right()
-        else:
-            if current._left_complete:
-                current = current.parent
-            current._left_complete = True
+    i = 0
+    s = list(_generate_balanced_string(n))
+
+    def recurse():
+        nonlocal i
+        if i >= len(s) or not s[i]:
+            i += 1
+            return None
+        i += 1
+        node = Node()
+        node.left = recurse()
+        if i >= len(s) or not s[i]:
+            i += 1
+            return node
+        node.right = recurse()
+        return node
+
+    root = recurse()
     relabel(root)
     return root
 
 
 if __name__ == '__main__':
-    print(list(random_bst(5).inorder()))
+    print(list(random_bst(5).preorder()))
     from collections import Counter
     c = Counter()
     for _ in range(10000):
-        c[random_balanced_string(5)] += 1
+        c[tuple(random_bst(5).preorder())] += 1
     print(c)
     print(len(c))
     t = Node()
-    t.insert_left()
-    t.left.insert_left()
-    t.insert_right()
-    t.right.insert_right()
-    t.right.insert_left()
-    t.right.left.insert_right()
-    print(t.inorder())
-    relabel(t)
-    print(t.inorder())
