c5f32de11f8cd684ab26dbff88551c77fa704434
Caleb Levy <caleb.levy@gmail.com>
2021-09-19 18:55:51 -0700

    Simplify

--- randombst.py
+++ randombst.py
@@ -4,21 +4,20 @@ import random
 def _generate_balanced_string(n):
     k = 2*n
     r = 0
-    while r != k:
-        if r == 0 or random.uniform(0, 1) >= r*(r+k+2)/(2*k*(r+1)):  # open
+    s = []
+    while k > 0:
+        if k > r and (r == 0 or random.uniform(0, 1) >= r*(r+k+2)/(2*k*(r+1))):
             yield "("
             r += 1
         else:
             yield ")"
             r -= 1
         k -= 1
-    while k > 0:
-        k -= 1
-        yield ")"
 
 
 def random_balanced_string(n):
     return "".join(_generate_balanced_string(n))
+
 
 
 def random_bst(n):
@@ -34,6 +33,6 @@ def random_bst(n):
 if __name__ == '__main__':
     from collections import Counter
     c = Counter()
-    for _ in range(4):
+    for _ in range(100000):
         c[random_balanced_string(3)] += 1
     print(c)
