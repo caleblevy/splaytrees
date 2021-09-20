7f106abbdb287234bfa7b52aa8fdd34463875bc0
Caleb Levy <caleb.levy@gmail.com>
2021-09-19 22:28:06 -0700

    Not promising

--- randombst.py
+++ randombst.py
@@ -6,25 +6,52 @@ CLOSED = True
 
 class Node:
 
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.value = value
+        self.parent = None
         self.left = None
         self.right = None
+        self._filled = False
 
-    def preorder(self):
-        return list(preorder(self.root))
+    def insert_left(self, value=None):
+        assert self.left is None
+        node = Node(value)
+        node.parent = self
+        self.left = node
+        return node
+
+    def insert_right(self, value=None):
+        assert self.right is None
+        node = Node(value)
+        node.parent = self
+        self.right = node
+        return node
+
+    def inorder(self):
+        return list(inorder(self))
 
 
-def preorder(node):
+def relabel(node, start=0):
     if node is not None:
-        yield from preorder(node.left)
+        start = relabel(node.left, start) + 1
+        node.value = start
+        start = relabel(node.right, start)
+    return start
+    
+
+
+def inorder(node):
+    if node is not None:
+        yield from inorder(node.left)
         yield node.value
-        yield from preorder(node.right)
+        yield from inorder(node.right)
 
 
 def _generate_balanced_string(n):
     k = 2*n
     r = 0
+    v = 1
+    root = current = None
     while k > 0:
         if k > r and (r == 0 or random.uniform(0, 1) >= r*(r+k+2)/(2*k*(r+1))):
             yield True
@@ -43,7 +70,17 @@ if __name__ == '__main__':
     # print(list(preorder(random_bst(5))))
     from collections import Counter
     c = Counter()
-    for _ in range(1000):
+    for _ in range(10000):
         c[random_balanced_string(5)] += 1
     print(c)
     print(len(c))
+    t = Node()
+    t.insert_left()
+    t.left.insert_left()
+    t.insert_right()
+    t.right.insert_right()
+    t.right.insert_left()
+    t.right.left.insert_right()
+    print(t.inorder())
+    relabel(t)
+    print(t.inorder())
