7a5340430cec742ecbc3d8674b2dc597f1cf33ef
Caleb Levy <caleb.levy@gmail.com>
2021-09-19 21:57:43 -0700

    Cleanup

--- randombst.py
+++ randombst.py
@@ -22,48 +22,9 @@ def preorder(node):
         yield from preorder(node.right)
 
 
-def random_bst(n):
-    """Uniform random binary search tree of size n."""
-    parents = []
-    has_left = []
-    root = None
-    s = [True]
-    s.extend(_generate_balanced_string(n))
-    s.append(False)
-    v = 1
-    i = 0
-    while i < len(s):
-        if s[i]:
-            i += 1
-            if not s[i]:
-                # mark left child as handled for this node
-                has_left.pop()
-                has_left.append(True)
-            else:
-                node = Node(v)
-                v += 1
-                if parents:
-                    if has_left[-1]:
-                        parents[-1].right = node
-                    else:  # left child hasn't been handled yet
-                        parents[-1].left = node
-                        has_left.pop()
-                        has_left.append(True)
-                else:  # no parent found; make this the root
-                    root = node
-                parents.append(node)
-                has_left.append(False)
-        else:
-            parents.pop()
-            has_left.pop()
-        i += 1
-    return root
-
-
 def _generate_balanced_string(n):
     k = 2*n
     r = 0
-    s = []
     while k > 0:
         if k > r and (r == 0 or random.uniform(0, 1) >= r*(r+k+2)/(2*k*(r+1))):
             yield True
@@ -72,14 +33,6 @@ def _generate_balanced_string(n):
             yield False
             r -= 1
         k -= 1
-
-
-def _relabel(node, start=0):
-    if node is not None:
-        start = _relabel(node, start) + 1
-        node.value = start
-        start = _relabel(node, start)
-    return start
 
 
 def random_balanced_string(n):
